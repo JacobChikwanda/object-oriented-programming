a78b174c4001a5fef78d5933e2721b432ed88e4a
Jacob Chikwanda <jacofaithcob@gmail.com>
2024-08-14 12:49:19 +0200

    Added first two concepts of python programming language

---
+++ python-quick-crash-course/000-python-syntax-and-basics/main.py
@@ -0,0 +1,21 @@
+'''
+main.py is a file dedicated to demonstrate the following concepts in python:
+a. How to print information
+b. How to use comments (single and multiline)
+c. How to use variables
+'''
+
+# a
+print("Hello, World!")
+
+# c
+name = "Jacob Chikwanda"
+age = 50
+height = 6.5
+isStudent = False
+isInstructor = True
+
+print(name, age, height)
+
+print(f'Name: {name}\nAge: {age}\nHeight: {height}\nIs Student: {isStudent}\nIs Instructor: {isInstructor}')
+print(f'Name Type: {type(name)}\nAge Type: {type(age)}\nHeight Type: {type(height)}\nIs Student Type: {type(isStudent)}\nIs Instructor: {type(isInstructor)}')
---
+++ python-quick-crash-course/001-basic-input-and-output/main.py
@@ -0,0 +1,17 @@
+'''
+main.py is a file dedicated to demonstrate the following concepts in python:
+a. How to print information
+b. How to collect information from a user
+c How to convert types
+'''
+
+# a
+# print("Please enter your name:")
+num1 = float(input("Please enter num 1:"))
+num2 = float(input("Please enter num 2:"))
+
+# Adding two numbers
+sum = int(num1) + int(num2)
+
+# Printing the sum of two numbers
+print(f'{num1} + {num2} = {sum}')
